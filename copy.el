b3b6c8ae01760a5f06dcbd8762a50625bc82e20d
zhangyuanyuan <yuan.zhang@zhangmen.com>
2019-07-04 00:01:21 +0800

    add beginning-of-buffer

--- copy.el
+++ copy.el
@@ -10,3 +10,22 @@
       (insert-buffer-substring buffer start end)
       (setq newmark (point)))
     (push-mark newmark)))
+
+
+(defun my-beginning-of-buffer (&optional arg)
+  (interactive "P")
+  (push-mark (point))
+  (goto-char (if arg
+                 (/ (* (point-max) (prefix-numeric-value arg)) 10)
+               (point-min))))
+
+;; 以上是有问题的
+
+(defun my-buffer-top (&optional arg)
+  (interactive "P")
+  (goto-char (if arg
+                 (if (> (buffer-size) 10000)
+                     (* arg (/ (buffer-size) 10))
+                   (/ (* (buffer-size) (prefix-numeric-value arg)) 10))
+               (point-min)))
+  (if arg (forward-line 1)))
